baccee8366c84159df415588d257c5f4b6e9ce9c
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-10-30 23:39:43 +0530

    Simple star pattern created

---
+++ Java Basics/SimplePattern.java
@@ -0,0 +1,10 @@
+public class SimplePattern {
+    public static void main(String argss[]){
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+    
+}
