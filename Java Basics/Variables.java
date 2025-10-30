5ce60b3a8ccc1ad8f167c7d80898c5ef0cbef186
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-10-31 00:27:56 +0530

    Variables in JAVA

---
+++ Java Basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Rohit Sharma";
+        System.out.println(name);
+
+        a = 50;
+        System.out.println(a);
+
+        a = b;
+        System.out.println(a);
+    }
+}
