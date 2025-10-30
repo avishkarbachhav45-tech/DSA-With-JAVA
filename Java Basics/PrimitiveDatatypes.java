cd5161f0807038e54b568c22be47bb6dc10536b0
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-10-31 01:16:36 +0530

    Primitive Datatypes in JAVA

---
+++ Java Basics/PrimitiveDatatypes.java
@@ -0,0 +1,32 @@
+public class PrimitiveDatatypes {
+    public static void main(String args[]){
+
+        // Primitive Datatypes in Java
+        byte a = 10;
+        System.out.println(a);
+
+        short b = 12;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean t = true;
+        System.out.println(t);
+        boolean f = false;
+        System.out.println(f);
+        
+        int num = 45;
+        System.out.println(num);
+
+        long l = 93774518;
+        System.out.println(l);
+
+        float price = 10.50f;
+        System.out.println(price);
+
+        double d = 99.93774555523434455;
+        System.out.println(d);
+
+    }
+}
