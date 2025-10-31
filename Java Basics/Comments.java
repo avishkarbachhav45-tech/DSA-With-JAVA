aeb091e17da18a1aa4f7b16008d46587c8763bf1
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-10-31 15:10:01 +0530

    Simple Sum Of program and Comments in Java

---
+++ Java Basics/Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+    public static void main (String args[]){
+
+        // Single line Comments
+        // Hello
+        // This is Rohit Sharma
+
+
+        /* Multiline Comments
+        Hello !
+        This is Rohit Sharma
+        Captain of Indian Team
+        */
+    }
+
+}
---
+++ Java Basics/SumOf.java
@@ -0,0 +1,9 @@
+public class SumOf {
+    public static void main(String args[])
+    {
+        int a = 30;
+        int b = 15;
+        int Rohit = a+b;
+        System.out.println(Rohit);
+    }
+}
