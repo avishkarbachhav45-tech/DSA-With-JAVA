6dbbd00d19a4b6c9802363bceedb8b23fe431edc
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-10-31 16:48:06 +0530

    Java programs for Inputs from user

---
+++ Java Basics/Inputs.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Inputs {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String fname = sc.nextLine();
+        // System.out.println(fname);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
+
+        // byte b = sc.nextByte();
+        // System.out.println(b);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // double db = sc.nextDouble();
+        // System.out.println(db);
+
+        // boolean bool = sc.nextBoolean();
+        // System.out.println(bool);
+
+        long lon = sc.nextLong();
+        System.out.println(lon);
+
+    }
+}
