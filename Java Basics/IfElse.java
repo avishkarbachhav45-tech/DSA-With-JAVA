c72011bc0b3df48cd69ef360806b1187b7b81d8b
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-01 22:02:06 +0530

    Conditional Statement - if else, else if and example of Even and Odd in java

---
+++ Java Basics/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main(String[] args) {
+        
+        int age = 16;
+        if(age>=18){
+            System.out.println("Adult : Drive, Vote");
+        }
+        if(age>13 && age<18){
+            System.out.println("Teenager");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+    
+}
