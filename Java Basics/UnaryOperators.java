5069d79bbed28987c2e9dfbb553350af4c8601e0
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-01 16:09:49 +0530

    Arithmetic, Unary and Relational operators in Java

---
+++ Java Basics/UnaryOperators.java
@@ -0,0 +1,31 @@
+public class UnaryOperators {
+    public static void main(String args[]) {
+        // Increment operator
+        // Pre Increment ++a
+        int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        
+        // Increment operator
+        // Post Increment c++
+        int c = 10;
+        int d = c++;
+        System.out.println(c);
+        System.out.println(d);
+
+        // Decrement operator
+        // Pre Decrement --p
+        int p = 10;
+        int q = --p;
+        System.out.println(p);
+        System.out.println(q);
+
+        // Decrement operator
+        // Post Decrement x--
+        int x = 10;
+        int y = x--;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
