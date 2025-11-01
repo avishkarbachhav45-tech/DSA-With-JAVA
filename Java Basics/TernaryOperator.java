86232cfc8fae8a6fc593a45c8106dce46f4aca8f
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-01 22:55:40 +0530

    Ternary Operator in java

---
+++ Java Basics/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int num = 4;
+
+        String type = (num % 2 == 0) ? "even" : "odd";
+        System.out.println(type);
+    }
+}
