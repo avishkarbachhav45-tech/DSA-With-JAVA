d3cfd9c5657768718e966cba2435511c10da0ccc
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-01 15:20:27 +0530

    Type casting and Type Conversion in Java

---
+++ Java Basics/TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting {
+    public static void main(String args[]){
+        float number = 99.999f;
+        int number2 = (int) number;
+        System.out.println(number2);
+
+        char ch = 'a';
+        int num = ch;
+        char ch2 = 'b';
+        int num2 = ch2;
+        System.out.println(num);
+        System.out.println(num2);
+    }
+}
---
+++ Java Basics/TypeConversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TypeConversion {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextFloat();
+        System.out.println(number);
+
+        sc.close();
+    }
+}
