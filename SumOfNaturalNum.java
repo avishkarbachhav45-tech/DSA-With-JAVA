629fb9e528119b79db3aac418f6ee1fa078b2e55
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-02 22:54:53 +0530

    While and For in JAVA  with  example

---
+++ SumOfNaturalNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SumOfNaturalNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int i = 1;
+        while (i<=n) {
+            sum += i;
+            i++;
+        }
+    System.err.println(sum);
+
+    sc.close();
+    }
+}
---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 45){
+            System.out.println("Rohit Sharma");
+            counter++;
+        }
+        System.out.println("Printed RS 45x");
+    }
+}
