c4eda7046be89d69a55c243a0d8fd9139a639040
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-04 23:05:13 +0530

    Call by value, examples of functions in java

---
+++ Java-Basics/Functions/ProductOf.java
@@ -0,0 +1,12 @@
+public class ProductOf {
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 5;
+        int prod = multiply(a, b);
+        System.out.println("a * b is :" + prod);
+    }
+}
