85872c10bd781ff5d1db9bc8e740dbe134026396
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-04 13:25:37 +0530

    Factorial of a number example in Java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int fact = 1;
+
+        System.out.println("Enter any positive integer :");
+        num = sc.nextInt();
+
+        for(int i = 1; i<=num; i++){
+            fact *=1;
+        }
+        System.out.println("Factorial :" + fact);
+
+        sc.close();
+    }
+    
+}
