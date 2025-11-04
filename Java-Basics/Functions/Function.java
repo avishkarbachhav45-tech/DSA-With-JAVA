060b7d86a1e24d11d99ec513f134f7fe5850922b
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-04 18:47:33 +0530

    Functions(methods) and Functions with Parameters in java

---
+++ Java-Basics/Functions/Function.java
@@ -0,0 +1,10 @@
+public class Function {
+    public static void RohitS(){
+        System.out.println("Rohit Sharma");
+        System.out.println("Rohit Sharma");
+        System.out.println("Rohit Sharma");
+    }
+    public static void main(String[] args) {
+        RohitS();
+    }
+}
---
+++ Java-Basics/Functions/FunctionwithParam.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FunctionwithParam {
+    public static int CalculateSum(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+        int sum = CalculateSum(a, b);
+        System.out.println("sum is :" + sum);
+
+
+        sc.close();
+    }
+}
