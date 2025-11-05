540a8b2ddb010b59ec4235504d972b48b72e0f19
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-05 15:56:18 +0530

    Method scope and Block scope in Java

---
+++ Java-Basics/Functions/BlockScope.java
@@ -0,0 +1,10 @@
+public class BlockScope {
+    public static void main(String[] args) {
+        
+        for(int i =1; i<= 5; i++){
+            System.out.println(i);
+        }
+        int i =45;
+        System.out.println(i);
+    }
+}
---
+++ Java-Basics/Functions/MethodScope.java
@@ -0,0 +1,6 @@
+public class MethodScope {
+    public static void main(String[] args) {
+        int Rs = 45;
+        System.out.println(Rs);     
+    }
+}
