e381dc54a70139c394978f6437af455a6f51b776
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-06 22:51:24 +0530

    Floyds triangle,Zero-One Triangle and Butterfly patter in java

---
+++ Java-Basics/Patterns-Part-II/Butterfly.java
@@ -0,0 +1,34 @@
+public class Butterfly {
+    public static void butterfly(int n){
+        //First half
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Second half
+        for(int i=n; i>=i; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        butterfly(4);
+    }
+}
