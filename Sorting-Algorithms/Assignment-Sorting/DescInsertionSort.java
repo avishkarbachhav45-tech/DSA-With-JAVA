7b701ef18f0ef1edfbba8aba2531237fdcab93d8
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-18 09:01:27 +0530

    Assignment on Sorting : sort an array in DESCENDING order

---
+++ Sorting-Algorithms/Assignment-Sorting/DescInsertionSort.java
@@ -0,0 +1,30 @@
+public class DescInsertionSort {
+    public static void  insertionSort(int arr[]){
+        
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while(prev>=0 && arr[prev]<curr){ // change the sign
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        insertionSort(arr);
+        printArr(arr);
+    }
+}
+
