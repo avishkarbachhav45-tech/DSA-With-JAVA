5dcde1481bcff04223cdd87a4722146b0d728b38
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-18 17:01:12 +0530

    Strings in java

---
+++ Java-Strings/Strings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("wxyzdhjsa434$#$%^^");
+
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+        System.out.println(str);
+        System.out.println(arr);
+        System.out.println(str2);
+
+        sc.close();
+    }
+}
