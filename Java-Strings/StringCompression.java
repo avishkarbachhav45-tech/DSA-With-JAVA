40bb69c83f89156ce34f98bf501e278a701e5000
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-19 21:38:42 +0530

    String Builder and Converting string to Uppercase

---
+++ Java-Strings/StringCompression.java
@@ -0,0 +1,11 @@
+public class StringCompression {
+
+    public static String compress(String str){
+        
+    }
+
+
+    public static void main(String[] args) {
+        String str = "aaabbcccdd";
+    }
+}
---
+++ Java-Strings/StringsBuilder.java
@@ -0,0 +1,13 @@
+public class StringsBuilder {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+
+        for(char ch='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb.length());
+        System.out.println(sb);
+    }
+    
+}
