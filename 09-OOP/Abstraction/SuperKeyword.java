ea1c54231a6af56d02e63c3f9b3e345ee2bff31c
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-25 11:10:12 +0530

    Static keyword and Super keyword

---
+++ 09-OOP/Abstraction/SuperKeyword.java
@@ -0,0 +1,22 @@
+public class SuperKeyword {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println(h.color);
+    }
+}
+
+class Animal{
+    String color;
+
+    Animal(){
+        System.out.println("Animal constructor called");
+    }
+}
+
+class Horse extends Animal{
+
+    Horse(){
+        super.color = "Brown";
+        System.out.println("Animal constructor called");
+    }
+}
