7529fb9ae74f19b575abc30f5261b17ea1d39dba
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-27 10:36:02 +0530

    Recursion problems - print binary strings

--- 10-Recursion/Part-1/Isorted.java
+++ 10-Recursion/Part-1/Isorted.java
@@ -8,7 +8,7 @@ public class Isorted {
             return false;
         }
 
-        return isSorted(arr, i);
+        return isSorted(arr, i+1);
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9};
